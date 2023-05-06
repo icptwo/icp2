5b3c487bb78c56f42eb3861ea2a4733578901c5e
Windows-LInux <yunglu@purdue.edu>
2023-05-06 07:37:48 -0400

    clarify

--- Part2/CH15/printpartition.c
+++ Part2/CH15/printpartition.c
@@ -5,7 +5,8 @@
 void printPartition(int * arr, int length)
 {
   int ind;
-  for (ind = 0; ind < length - 1; ind ++) { printf("%d + ", arr[ind]); }
+  for (ind = 0; ind < length - 1; ind ++)
+    { printf("%d + ", arr[ind]); }
   printf("%d\n", arr[length - 1]);
 }
 void partition(int * arr, int ind, int left)
@@ -16,10 +17,11 @@ void partition(int * arr, int ind, int left)
       printPartition(arr, ind);
       return; // not necessary
     }
-  for (val = 1; val <= left; val ++)
+  for (val = 1; val <= left; val ++) // start at 1, not 0
     {
       arr[ind] = val;
       partition(arr, ind + 1, left - val);
+      // RL1 (return location 1)
     }
 }
 int main(int argc, char * argv[])
@@ -30,6 +32,7 @@ int main(int argc, char * argv[])
   int * arr;
   arr = malloc(sizeof(int) * n);
   partition(arr, 0, n);
+  // RL2 (return location 2)
   free (arr);
   return EXIT_SUCCESS;
 }
