ecdd686d5eee8fd0fc96ed5ec2da04cea3dbc3b1
Windows-LInux <yunglu@purdue.edu>
2023-05-07 09:37:44 -0400

    add files

---
+++ Part3/CH17/personprint.c
@@ -0,0 +1,8 @@
+// CH17:personprint.c
+#include "person.h"
+#include <stdio.h>
+void Person_print(Person * p)
+{
+  printf("Name: %s. ", p -> name);
+  printf("Date of Birth: %d/%d/%d\n", p -> year, p -> month, p -> date);
+}
